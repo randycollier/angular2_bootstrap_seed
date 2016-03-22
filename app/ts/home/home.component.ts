1af500922f06bf301c288e5cc5d94c5eddd7dadc
rcollier <rcollier@gwii.com>
2016-03-22 14:19:09 -0500

    String change.

---
+++ app/ts/home/home.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'home',
+	templateUrl: 'app/templates/home.component.html'
+
+})
+
+export class HomeComponent {
+	title: string;
+	constructor(){
+		this.title='It is ?? ';
+	}
+}
+
